89731fb04ab7f275aa0fb613e9f74b9b46b1b997
pooyan <thepooyan@gmail.com>
2025-12-14 15:42:54 +0330

    fixed sql problems

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,19 @@
 import { primaryKey, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const foodTable = sqliteTable("food", s => ({
-  id: s.integer().primaryKey({ autoIncrement: true }).notNull(),
+export const ingridientsTable = sqliteTable("ingridients", s => ({
+  id: s.integer({ mode: "number" }).primaryKey({ autoIncrement: true }).notNull(),
   name: s.text().notNull(),
-  ingridients: s.integer().references(() => ingridientsTable.id)
 }));
 
-export const ingridientsTable = sqliteTable("ingridients", s => ({
-  id: s.integer().primaryKey({ autoIncrement: true }).notNull(),
+export const foodTable = sqliteTable("food", s => ({
+  id: s.integer({ mode: "number" }).primaryKey({ autoIncrement: true }).notNull(),
   name: s.text().notNull(),
 }));
+
 
 export const food_ingridientsTable = sqliteTable("food_ingridients", s => ({
-  foodId: s.integer().references(() => foodTable.id).notNull(),
-  ingridientId: s.integer().references(() => ingridientsTable.id).notNull(),
+  foodId: s.integer({mode: "number"}).references(() => foodTable.id).notNull(),
+  ingridientId: s.integer({mode: "number"}).references(() => ingridientsTable.id).notNull(),
   amount: s.text()
 }), 
 (table) => [
