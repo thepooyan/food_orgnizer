7614a8b05f79749a7d39c98b816ecb26642e75a7
pooyan <thepooyan@gmail.com>
2025-12-14 12:08:26 +0330

    update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,22 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { primaryKey, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const foodTable = sqliteTable("food", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
-});
+export const foodTable = sqliteTable("food", s => ({
+  id: s.integer().primaryKey({ autoIncrement: true }).notNull(),
+  name: s.text().notNull(),
+  ingridients: s.integer().references(() => ingridientsTable.id)
+}));
+
+export const ingridientsTable = sqliteTable("ingridients", s => ({
+  id: s.integer().primaryKey({ autoIncrement: true }).notNull(),
+  name: s.text().notNull(),
+}));
+
+export const food_ingridientsTable = sqliteTable("food_ingridients", s => ({
+  foodId: s.integer().references(() => foodTable.id).notNull(),
+  ingridientId: s.integer().references(() => ingridientsTable.id).notNull(),
+  amount: s.text()
+}), 
+(table) => [
+  primaryKey({columns: [table.foodId, table.ingridientId]})
+]
+)
