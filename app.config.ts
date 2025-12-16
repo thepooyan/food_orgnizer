a16e571eb48926c7e9495bf31ba13846a4f4d59c
pooyan <thepooyan@gmail.com>
2025-12-16 21:10:55 +0330

    nothing!

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
+import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   vite: {
     ssr: { external: ["drizzle-orm"] },
      plugins: [
       tailwindcss(),
-    ],
+      viteTsconfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ]
   },
 });
