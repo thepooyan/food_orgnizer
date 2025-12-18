d04d746ab9dd178792213c450a4d8374fc6d8f36
pooyan <thepooyan@gmail.com>
2025-12-18 14:42:00 +0330

    nothing!

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,8 +5,8 @@ import { getFoods, getIngridients } from "~/db/DataFetching"
 
 export const foodQuery = query(async () => {
     return getFoods()
-}, "food")
+}, "foods")
 
 export const ingridientsQuery = query(async () => {
     return getIngridients()
-}, "food")
+}, "ingridients")
